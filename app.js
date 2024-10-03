ae5e1b5c2d548cd67827cb725e3ebfd665654e2f
Sean Bain <artemis45566@gmail.com>
2024-10-03 06:32:26 +0100

    login

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 
+const bcrypt = require('bcrypt');
+
 const mongoose = require('mongoose');
 
 const session = require('express-session');
@@ -34,6 +36,25 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 
+
+const sessionStore = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: 'sessions'
+});
+
+
+app.use(session({
+    secret: 'your secret key',
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 10
+    }
+}));
+
+
+
 const fileSchema = new mongoose.Schema({
     fileContent: Buffer,
     contentType: String,
@@ -45,6 +66,25 @@ const fileSchema = new mongoose.Schema({
 const UploadedFile = mongoose.model('UploadedFile', fileSchema);
 
 
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+
+
+
+
+
+
+
+
+
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -54,8 +94,107 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.get('/', (req, res) => {
 
 
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
+
+app.get('/login', (req, res) => {
+
+
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/signup.html'));
+})
+
+
+
+
+
+app.post('/signup', async (req, res) => {
+
+    try {
+        const { username, password } = req.body;
+        const existingUser = await User.findOne({ username });
+
+        if (existingUser) {
+            return res.status(409).send('Username already taken');
+        }
+
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User ({
+            username,
+            password: hashedPassword
+        });
+
+        await user.save();
+        res.redirect('/login'); // Redirect to login page after signup
+    } catch (error) {
+
+        console.error('Signup failed', error);
+        res.status(500).send('Error signing up');
+    }
+});
+
+
+
+
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+
+        
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).send('User not found');
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+
+            return res.status(401).send('Invalid credentials');
+        }
+
+
+        // Creating a session
+        req.session.userId = user._id;
+        req.session.username = user.username;
+
+        res.cookie('username', user.username, { httpOnly: false, secure: true });
+
+
+        res.redirect('/'); // Redirect to a protected route after login
+    } catch (error) {
+        console.error('Login error', error);
+        res.status(500).send('Error loggin in');
+    }
+});
+
+
+
+// protect certain routes from non-logged in users
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.userId) {
+        return res.redirect('/login');
+    }
+    next();
+};
+
+
+app.post('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send('Failed to logout');
+        }
+        res.redirect('/');
+    });
+});
+
 
 
 
@@ -106,9 +245,6 @@ app.get('/download/:id', async (req, res) => {
 app.get('/files', async (req, res) => {
     
 
-
-
-
     try {
         const files = await UploadedFile.find({}, 'fileName _id').lean();
         res.json(files);
@@ -116,9 +252,6 @@ app.get('/files', async (req, res) => {
         console.error('Failed to retrieve files:', error);
         res.status(500).send('Error fetching file list');
     }
-
-
-
 
 });
 
